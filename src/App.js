cbaad583d2a9242a4e19aa5bb246611d24609308
BryanCl <clvn.intel@gmail.com>
2020-05-13 11:51:16 +0300

    code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from "react";
 import db from "./firebaseConfig";
 
-db.collection("users")
-  .add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815,
-  })
-  .then(function (docRef) {
-    console.log("Document written with ID: ", docRef.id);
-  })
-  .catch(function (error) {
-    console.error("Error adding document: ", error);
-  });
-
 function App() {
+  const firestoreHandler = () => {
+    db.collection("users")
+      .add({
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815,
+      })
+      .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+  };
+
   return (
     <div>
       <input type="file" name="assignments" />
-      <button className="btn btn-primary" name="Submit">
+      <button
+        className="btn btn-primary"
+        name="Submit"
+        onchange={firestoreHandler}
+      >
         submit
       </button>
     </div>
